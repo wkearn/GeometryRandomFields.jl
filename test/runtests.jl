fcaf1b900b4757fdf5e647179bb43784e83f2226
Will Kearney <wkearn@gmail.com>
2016-03-18 09:55:26 -0400

    Add basic tests of covariance, generator, acf

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
 using GeometryRandomFields
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+GRF = GeometryRandomFields
+
+gf = GRF.GaussianCovarianceFunction(10,1)
+@test gf(0) == 1
+@test_approx_eq gf(10) exp(-1/2)
+
+mf = GRF.MatérnCovarianceFunction(1/2,10,1)
+@test mf(0) == 1
+@test_approx_eq mf(10) exp(-1)
+
+m,n = 1024,512
+M = GRF.grf(m,n,gf)
+@test size(M) == (2n,2m)
+
+GRF.acf(real(M))
